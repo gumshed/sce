b7029087b7a5ad2ebc58e358ced589072b7335c5
gumshed <gumshed@github.com>
2019-02-16 17:17:37 -0700

    UpdatesUpdates

---
+++ 0x-launch-kit/js/config.js
@@ -0,0 +1,27 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const _0x_js_1 = require('0x.js');
+// Network port to listen on
+exports.HTTP_PORT = 3000;
+// A time window after which the order is considered permanently expired
+exports.ORDER_SHADOWING_MARGIN_MS = 100 * 1000; // tslint:disable-line custom-no-magic-numbers
+// Frequency of checks for permanently expired orders
+exports.PERMANENT_CLEANUP_INTERVAL_MS = 10 * 1000; // tslint:disable-line custom-no-magic-numbers
+// Max number of entities per page
+exports.MAX_PER_PAGE = 1000;
+// Default network id to use when not specified
+exports.NETWORK_ID = 3;
+// An array of fee recipients
+exports.FEE_RECIPIENT = '0x3C945fe4366A5a3E4D3cb841BE6459CF3d2063A2';
+// A flat fee in ZRX that should be charged to the order maker
+exports.MAKER_FEE_ZRX_UNIT_AMOUNT = new _0x_js_1.BigNumber(0);
+// A flat fee in ZRX that should be charged to the order taker
+exports.TAKER_FEE_ZRX_UNIT_AMOUNT = new _0x_js_1.BigNumber(0);
+// Whitelisted token addresses. Set to a '*' instead of an array to allow all tokens.
+exports.WHITELISTED_TOKENS = [
+    '0x3d46454212c61ecb7b31248047fa033120b88668',
+];
+// Ethereum RPC url
+exports.RPC_URL = 'https://kovan.infura.io/v3';
+// Default ERC20 token precision
+exports.DEFAULT_ERC20_TOKEN_PRECISION = 18;
